f20f080832a98f5953f781330741a91cb312bb82
alymPp <urozmatovalymbek@gmail.com>
2025-04-13 23:58:10 +0600

    Add files via upload

---
+++ JavaScript/a-seo.js
@@ -0,0 +1,25 @@
+type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Service",
+  "name": "Услуги филиппинских нянь в Бишкеке",
+  "provider": {
+    "@type": "Organization",
+    "name": "Global Staff Agency",
+    "telephone": "+996555990277",
+    "email": "globalstaffagencykg@gmail.com",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "Аалы Токомбаева 45/1",
+      "addressLocality": "Бишкек",
+      "addressCountry": "KG"
+    }
+  },
+  "description": "Подбор профессиональных нянь из Филиппин для семей в Бишкеке",
+  "areaServed": {
+    "@type": "City",
+    "name": "Бишкек"
+  },
+  "serviceType": ["Подбор филиппинских нянь", "Трудоустройство домработниц", "Услуги домашнего персонала"],
+  "url": "https://globalstaff.agency"
+}
